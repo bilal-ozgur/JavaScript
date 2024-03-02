59ebddc5a004aa2464032a723a479eb4309d5ce1
Bilal Talha Özgür <130503711+bilal-ozgur@users.noreply.github.com>
2024-03-02 21:58:19 +0100

    Add files via upload

---
+++ JAVASCRIPT CLASS-IN/JS02-Operators/operators.js
@@ -0,0 +1,58 @@
+console.log("hello world");
+
+firstName = "Bilal";
+surnameName = "Ozgur";
+const fullname2 = `${firstName} ${surnameName}`;
+console.log(fullname2);
+
+console.log(`${firstName} ${surnameName}`);
+
+const num1 = "5";
+const num2 = "7";
+
+console.log(num1 + num2);
+console.log(num1 + 7);
+console.log(num1 + "7");
+
+console.log(typeof (7 + 3));
+console.log(typeof (num1 + 7));
+
+console.log(num2 - num1);
+
+console.log("5" * 6);
+
+let num3 = 5;
+console.log(num3++);
+
+console.log(num3);
+
+let num4 = 8;
+num4++;
+
+console.log(num4);
+
+let num5 = 7;
+console.log(num5++);
+
+let num6 = 10;
+console.log(num6++);
+
+console.log("A".charCodeAt());
+console.log("a".charCodeAt());
+console.log("a" > "A");
+console.log("5".charCodeAt());
+console.log("-".charCodeAt());
+console.log("@".charCodeAt());
+console.log("a" > "@");
+
+console.log(15 >= 15);
+console.log("15" >= 15);
+// console.log(15>=='15'); //error verir
+
+console.log('');
+
+console.log(typeof '');
+
+console.log(0 && 'dunya');
+console.log('' || "merhaba");
+console.log('' && 'merhaba');
