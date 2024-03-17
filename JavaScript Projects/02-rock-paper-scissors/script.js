45b2265c00fe41b298e4abe8f9741bd3c0a9116e
Bilal Talha Özgür <130503711+bilal-ozgur@users.noreply.github.com>
2024-03-17 21:27:09 +0100

    project update

--- JavaScript Projects/02-rock-paper-scissors/script.js
+++ JavaScript Projects/02-rock-paper-scissors/script.js
@@ -1,37 +1,64 @@
+let score = JSON.parse(localStorage.getItem("score")) || {
+  wins: 0,
+  losses: 0,
+  ties: 0,
+};
+
+/*
+if (!score) {
+  score = {
+    wins: 0,
+    losses: 0,
+    ties: 0,
+  };
+}
+*/
+
 function playGame(playerMove) {
   const computerMove = pickComputerMove();
 
   let result = "";
-  
-  if (playerMove === 'scissors') {
+
+  if (playerMove === "scissors") {
     if (computerMove === "rock") {
       result = "Computer wins!";
     } else if (computerMove === "paper") {
       result = "You win!";
     } else if (computerMove === "scissors") {
       result = "Tie!";
-    } 
-
-  } else if (playerMove === 'paper') {
+    }
+  } else if (playerMove === "paper") {
     if (computerMove === "rock") {
       result = "You win!";
     } else if (computerMove === "paper") {
       result = "Tie!";
     } else if (computerMove === "scissors") {
-      result = "Computer wins!";}
-
-  } else if (playerMove === 'rock') {
+      result = "Computer wins!";
+    }
+  } else if (playerMove === "rock") {
     if (computerMove === "rock") {
-      result = 'Tie !'
-    } else if (computerMove === 'paper') {
-      result = 'Computer wins!'
-    } else if (computerMove === 'scissors') {
-      result = 'You win!'
+      result = "Tie!";
+    } else if (computerMove === "paper") {
+      result = "Computer wins!";
+    } else if (computerMove === "scissors") {
+      result = "You win!";
     }
   }
-  
-  alert(`You picked ${playerMove}. Computer picked ${computerMove}. ${result}`);
 
+  console.log(score);
+
+  if (result === "Tie!") {
+    score.ties += 1;
+  } else if (result === "You win!") {
+    score.wins += 1;
+  } else if (result === "Computer wins!") {
+    score.losses += 1;
+  }
+
+  localStorage.setItem("score", JSON.stringify(score));
+
+  alert(`You picked ${playerMove}. Computer picked ${computerMove}. ${result}
+Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`);
 }
 
 function pickComputerMove() {
